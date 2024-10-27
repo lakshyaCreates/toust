b7a77f8eedb4a9028f95431e996a3c04a2c443b4
lakshyaCreates <lakshya.creates07@gmail.com>
2024-10-27 21:30:16 +0530

    Clean Repo for Release 0.1.0

--- src/app/(routes)/(main)/websites/page.tsx
+++
@@ -1,8 +0,0 @@
-import { auth } from "@/auth";
-
-export default async function WebsitesPage() {
-    const session = await auth();
-    const user = session?.user;
-
-    return <div>{JSON.stringify(user)}</div>;
-}
--- src/types/auth-core.d.ts
+++
@@ -1,8 +0,0 @@
-import type { AdapterUser as DefaultAdapterUser } from "@auth/core/adapters";
-import { Plan } from "@prisma/client";
-
-declare module "@auth/core/adapters" {
-    interface AdapterUser {
-        plan: Plan;
-    }
-}
