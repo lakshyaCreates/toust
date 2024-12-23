6f580a83a7c5cdfba8ef589cc8655add7d3b07f9
lakshyaCreates <lakshya.creates07@gmail.com>
2024-12-24 00:54:05 +0530

    refactor: base code environment setup

---
+++ src/providers/index.tsx
@@ -0,0 +1,16 @@
+import { ThemeProvider } from "./theme-provider";
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+    return (
+        <>
+            <ThemeProvider
+                attribute="class"
+                defaultTheme="dark"
+                enableSystem
+                disableTransitionOnChange
+            >
+                {children}
+            </ThemeProvider>
+        </>
+    );
+}
